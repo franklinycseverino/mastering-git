1797b811c3d28228476d3ff44e4a9b09e5865c00
Franklin <franklincseverino@gmail.com>
2025-11-13 08:17:47 -0500

    add manifest from NEFFEX console.log

--- master/hello.js
+++ master/hello.js
@@ -2,3 +2,4 @@ console.log('Hello, Cole!');
 console.log('Hello, Donatello!')
 console.log('Hello! from dev-franklin');
 console.log('Hello, Anomaly. Welcome to Git!')
+console.log('Manifest (963hz) from NEFFEX is fire!')
